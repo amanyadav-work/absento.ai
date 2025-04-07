656e475d5a1645b99e7a46518b0df1d021d85e5b
amanyadav-work <amansyadavwork@gmail.com>
2025-04-07 18:50:58 +0530

    Update Express app for Vercel deployment and modify CORS config

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,12 +37,7 @@ const login = async (req, res) => {
             const token = jwt.sign({ email, role }, process.env.JWT_SECRET);
 
             // Set token in cookie and respond with success
-            res.cookie('jwttoken', token, {
-                httpOnly: false,  // Prevent JavaScript access to the cookie
-                secure: true,
-                sameSite: 'None',
-                domain: '.vercel.app',
-            });
+            res.cookie('jwttoken', token);
             res.status(201).send({ message: "Operation Successful" });
         });
     } catch (error) {
@@ -120,12 +115,7 @@ const register = async (req, res) => {
         const token = jwt.sign({ email, role }, process.env.JWT_SECRET);
 
         // Set token in cookie and respond with success
-        res.cookie('jwttoken', token, {
-            httpOnly: false,  // Prevent JavaScript access to the cookie
-            secure: true,
-            sameSite: 'None',
-            domain: '.vercel.app',
-        });
+        res.cookie('jwttoken', token);
         res.status(201).send({ message: "Operation Successful" });
 
     } catch (error) {
